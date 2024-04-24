c40327f90c3f3a38bf51f5f833aece75ede6b84b
EmreErdogdu <168023154+EmreErdogdu@users.noreply.github.com>
2024-04-24 22:27:01 +0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,1 +1,20 @@
-print("Hello World")
+            
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            "ROFL" : "bir şakaya karşılık cevap",
+            "SHEESH" : "onaylamamak",
+            "CREEPY" :" korkunç",
+            "AGGRO" : "agresifleşmek/sinirlenmek",
+            }
+            
+word = input("Anlamını bilmediğiniz bir kelime yazın (hepsini büyük harflerle yazın!): ")
+
+if word in meme_dict:
+    print(word,"kelimesinin anlamı=",meme_dict[word])
+    
+else:
+    print("Maalesef kelime sözlükte bulunmamakta")
+    
+    
+    
