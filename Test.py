005e73940f61c21093677e51edb891aabb0ad83b
EmreErdogdu <168023154+EmreErdogdu@users.noreply.github.com>
2024-05-15 22:20:14 +0300

    discord_bot.py

--- Test.py
+++ Test.py
@@ -1,20 +1,53 @@
-            
-meme_dict = {
-            "CRINGE": "Garip ya da utandırıcı bir şey",
-            "LOL": "Komik bir şeye verilen cevap",
-            "ROFL" : "bir şakaya karşılık cevap",
-            "SHEESH" : "onaylamamak",
-            "CREEPY" :" korkunç",
-            "AGGRO" : "agresifleşmek/sinirlenmek",
-            }
-            
-word = input("Anlamını bilmediğiniz bir kelime yazın (hepsini büyük harflerle yazın!): ")
+import discord
+from discord.ext import commands
 
-if word in meme_dict:
-    print(word,"kelimesinin anlamı=",meme_dict[word])
-    
-else:
-    print("Maalesef kelime sözlükte bulunmamakta")
-    
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def naber(ctx):
+    await ctx.send('iyiyim sen nasılsın?')
+
+
+@bot.command()
+async def toplama(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(f'sonuç={left + right}')
+
+@bot.command()
+async def çıkarma(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(f'sonuç={left - right}')
+
+@bot.command()
+async def çarpma(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(f'sonuç={left * right}')
+
+@bot.command()
+async def bölme(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(f'sonuç={left / right}')
+
+@bot.command()
+async def üst(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(f'sonuç=  {left ** right}')
+
+bot.run("YOUR DISCORD TOKEN")
     
     
